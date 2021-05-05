94caa5df441667d56d86347194929906ae6e82b6
Nacho <nachot311296@gmail.com>
2021-05-04 21:35:03 -0300

    clase JerseyConfig

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/config/JerseyConfig.java
@@ -0,0 +1,9 @@
+package ar.edu.unnoba.pdyc.mymusic.config;
+
+import org.apache.tomcat.util.file.ConfigurationSource;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JerseyConfig extends ResourceConfig {
+}
