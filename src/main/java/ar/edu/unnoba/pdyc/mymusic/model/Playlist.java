4f7d5c5d20def0995e69a1bde6cf3a96ea9d53da
Nacho <nachot311296@gmail.com>
2021-05-11 19:29:07 -0300

    desarrollo del model

---
+++ src/main/java/ar/edu/unnoba/pdyc/mymusic/model/Playlist.java
@@ -0,0 +1,57 @@
+package ar.edu.unnoba.pdyc.mymusic.model;
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "playlists")
+public class Playlist implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private User userId;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "playlists_songs",inverseJoinColumns = @JoinColumn(name = "song_id"))
+    private List<Song> songs;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUserId() {
+        return userId;
+    }
+
+    public void setUserId(User userId) {
+        this.userId = userId;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+}
